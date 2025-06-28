1aacd345eff0847ee5982857c3a0d301a4088377
nickolai <nicolas.lastra.olguin@gmail.com>
2025-06-28 18:37:25 -0400

    agregando ruta por defecto cuando se tipee cualquier ruta despues del /

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const dotenv = require('dotenv').config();
 const {connectDB} = require('./database/config');
 const cors = require('cors')
+const path = require('path')
 
 const PORT = process.env.PORT || 4000;
 
@@ -24,7 +25,9 @@ app.use(express.json());
 //TODO: auth // crear, login renew
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/events', require('./routes/events'));
-
+app.use('/*',(req, res) =>{
+    res.sendFile(path.join(__dirname, 'public/index.html'))
+})
 
 //configurar el puerto
 app.listen(PORT, () => {
